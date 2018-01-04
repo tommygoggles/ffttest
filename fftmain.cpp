f55b2b65193fc1507e6caa3196a9cc2c4c8770bd
Tom <tommygoggles@gmail.com>
2018-01-04 15:00:59 +0000

    Update fftmain.cpp

--- fftmain.cpp
+++ fftmain.cpp
@@ -7,7 +7,7 @@ const double PI = 3.141592653589793238460;
 typedef std::complex<double> Complex;
 typedef std::valarray<Complex> CArray;
 
-// Cooley–Tukey FFT (in-place, divide-and-conquer)
+// Cooleyâ€“Tukey FFT (in-place, divide-and-conquer)
 // Higher memory requirements and redundancy although more intuitive
 void fft(CArray& x)
 {
@@ -101,11 +101,14 @@ void ifft(CArray& x)
 int main()
 {
     //const Complex test[] = { 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0 };
-    const Complex test[] = { -1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0 };
+   // const Complex test[] = { -1.0, -1.0, 1.0, 1.0, -1.0, -1.0, 1.0, 1.0 };
+	
+	const Complex test[] = { 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+	
     CArray data(test, 8);
 
     // forward fft
-    fft(data);
+   // fft(data);
 
     std::cout << "fft" << std::endl;
     for (int i = 0; i < 8; ++i)
