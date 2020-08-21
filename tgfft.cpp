dfa5c8cf84528f2d404282f0fc6fe975efb21cf8
Tom Barker <tommygoggles@gmail.com>
2020-08-21 22:12:36 +0100

    more complex

--- tgfft.cpp
+++ tgfft.cpp
@@ -16,13 +16,21 @@ class cmplx
     cmplx(double real = 0, double imaginary = 0);
 
 
-    cmplx operator+ (const cmplx r) const;
-    cmplx operator+= (const cmplx r);
+
+    cmplx operator+ (const cmplx &r) const;
+    cmplx operator+= (const cmplx &r);
+    cmplx operator* (const cmplx &r) const;
+    cmplx operator*= (const cmplx &r);
+
+    void copyfrom(const cmplx &tocopy);
+    cmplx operator= (const cmplx toassign);
+    cmplx(const cmplx &tocopy);
 
     double getlength();
     double getangle();
 
 };
+
 
 cmplx::cmplx(double real, double imaginary)
 {
@@ -30,18 +38,47 @@ cmplx::cmplx(double real, double imaginary)
     im = imaginary;
 }
 
+void cmplx::copyfrom(const cmplx &tocopy)
+{
+    re = tocopy.re;
+    im = tocopy.im;
+}
 
-cmplx cmplx::operator+ (const cmplx r) const
+cmplx cmplx::operator= (const cmplx toassign)
+{
+    copyfrom(toassign);
+    return *this;
+}
+
+cmplx::cmplx(const cmplx &tocopy)
+{
+    copyfrom(tocopy);
+}
+
+
+cmplx cmplx::operator+ (const cmplx &r) const
 {
     return cmplx(re+r.re,im+r.im);
 }
 
-cmplx cmplx::operator+= (const cmplx r)
+cmplx cmplx::operator+= (const cmplx &r)
 {
     re+=r.re;
     im+=r.im;
     return *this;
 }
+
+cmplx cmplx::operator* (const cmplx &r) const
+{
+    return cmplx( (re*r.re)-(im*r.im), (re*r.im)+(im*r.re) );
+}
+
+cmplx cmplx::operator*= (const cmplx &r)
+{
+    copyfrom(*this*r);
+    return *this;
+}
+
 
 double cmplx::getlength()
 {
@@ -53,19 +90,22 @@ double cmplx::getangle()
     return atan2(im,re);
 }
 
-void fouriertransform(double* input, cmplx* output, int length)
+
+
+
+void fouriertransform(cmplx* input, cmplx* output, int length)
 {
+    double norm = (1.0/length);
     for(int k = 0;k<length;k++)
     {
         cmplx kval;
         for(int n = 0;n<length;n++)
         {
             double twopiknoverlength = (TWO_PI * k * n)/length;
-            kval+=cmplx(input[n]*cos(twopiknoverlength), -(input[n]*sin(twopiknoverlength)));
+            kval += input[n] * cmplx(cos(twopiknoverlength), -(sin(twopiknoverlength)));
+            //kval+=cmplx(input[n]*cos(twopiknoverlength), -(input[n]*sin(twopiknoverlength)));
         }
-        kval.re/=length;
-        kval.im/=length;
-        output[k] = kval;
+        output[k] = kval*norm;
     }
 }
 
@@ -79,26 +119,60 @@ void printdoubles(double* thedoubles, int length)
     printf("\r\n");
 }
 
+
+
+cmplx* getcomplex(double* theinput, int length)
+{
+    cmplx* output = new cmplx[length];
+    for(int i = 0;i<length;i++)
+    {
+        output[i].re = theinput[i];
+    }
+    return output;
+}
+
+
 void printall(double* theinput, int length)
 {
     printf("input: \r\n");
     printdoubles(theinput,length);
 
+    cmplx* complexinput = getcomplex(theinput,length);
+
     cmplx output[length];
-    fouriertransform(theinput,output,length);
+    fouriertransform(complexinput,output,length);
     double amplitudes[length];
     double phases[length];
     for(int i = 0;i<length;i++)
     {
         amplitudes[i] = output[i].getlength();
-        phases[i] = output[i].getangle()*rad2deg;
+        if(amplitudes[i] > 0.0000001)
+        {
+            phases[i] = output[i].getangle()*rad2deg;
+        }
+        else
+        {
+            phases[i] = 0;
+        }        
     }
     printf("amplitudes: \r\n");
     printdoubles(amplitudes,length);
     printf("phases: \r\n");
     printdoubles(phases,length);
 
+    cmplx backthroughoutput[length];
+    //mer??
+    fouriertransform(output,backthroughoutput,length);
+    //mer?
+    for(int i = 0;i<length;i++)
+    {
+        printf("%f, ",backthroughoutput[i].re);//, backthroughoutput[i].im);
+    }
+    printf("\r\n");
+
 }
+
+
 
 
 int main (int argc, const char* argv[])
